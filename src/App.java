0cfddf5a83f4b23928dbb124257258566cc96b2f
fabian-bejarano <fabian.bejarano21@gmail.com>
2021-07-31 14:29:59 -0500

    clase 05

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:hr.db");
+            if (connection != null){
+                System.out.println("Conexion exitosa");
+                
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e.getClass().getName()+ ":" +e.getMessage());
+            System.out.println("Error en la conexion");
+        }
+
     }
 }
