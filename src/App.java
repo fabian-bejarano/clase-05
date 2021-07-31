27c7c1620c56d386d6d1159f175e9e9d38436cf6
fabian-bejarano <fabian.bejarano21@gmail.com>
2021-07-31 16:14:46 -0500

    clase 05

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+
+import javax.naming.spi.DirStateFactory.Result;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,8 +18,22 @@ public class App {
                 System.out.println("Conexion exitosa");
                 
             }
+
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30); // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("Select * from employees");
+            while(rs.next())
+            {
+                // read the result set 
+                System.out.println("apellido = " + rs.getString("last_name"));
+                System.out.println("nombre = " + rs.getString("first_name"));
+                System.out.println("email = " + rs.getString("email"));
+            }
+
+
         } catch (Exception e) {
-            // TODO: handle exception
+            // Todo: handle exception
             System.err.println(e.getClass().getName()+ ":" +e.getMessage());
             System.out.println("Error en la conexion");
         }
